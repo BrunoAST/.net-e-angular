4e06836dc09dabb8fadea35c8f06d726ffce86ce
Bruno Alves <bruno-alves2010@live.com>
2019-07-07 23:40:56 -0300

    Photo model

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -1,4 +1,7 @@
-﻿namespace DatingApp.API.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatingApp.API.Models
 {
     public class User
     {
@@ -6,5 +9,16 @@
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnowAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introdoction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
